b16a89cdde370470197231222147e0e16a9502cf
aschi87 <ernst.leisi@hotmail.com>
2015-05-17 21:28:18 +0200

    test2

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,45 +1,6 @@
 function addToList() {
-
-    // Read the date and put it in the right format.
-    var datDate = $(datepicker).datepicker({
-        dateFormat: 'dd.mm.yyyy'
-    }).val();
-
-    var datPosten = $('#posten').val();
-    var datSelect = $('#select :selected').text();
-    var datBetrag = $('#betrag').val();
-
-
-    var table = document.getElementById("table");
-    var row = table.insertRow(1);
-    var datum = row.insertCell(0);
-    var posten = row.insertCell(1);
-    var kat = row.insertCell(2);
-    var betrag = row.insertCell(3);
-    var btnCell = row.insertCell(4);
-
-    //alert(table.rows.length);
-
-    datum.innerHTML = datDate;
-    posten.innerHTML = datPosten;
-    kat.innerHTML = datSelect;
-    betrag.innerHTML = datBetrag;
-
-    btnCell = $("#myButton").click(function () {
-        var test = $('<button/>', {
-            text: 'Test',
-            click: function () {
-                alert('hi');
-            }
-        });
-        //var parent = $('<tr><td></td></tr>').children().append(test).end();
-        //$("#table").before(parent);
-    });
-}
-
-function beDifferent() {
     var amountValue = $('#betrag').val();
-    var dateValue = $(datepicker).datepicker({
+    var datDate = $("#datepicker").datepicker({
         dateFormat: 'dd.mm.yyyy'
     }).val();
     var nameValue = $('#posten').val();
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,30 +4,21 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>Budget Planer</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link href="{{ asset('/css/app.css')}}" rel="stylesheet">
+    <link href="{{ asset('/css/datepicker.css')}}" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
-    <script src="{{ asset('js/modal.js') }}"></script>
-    <script>
-        $(function(){
-            $('#datepicker').datepicker();
-        });
-    </script>
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap-datepicker.js')}}"></script>
+    <script src="{{ asset('js/modal.js')}}"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -75,8 +66,5 @@
 
 	@yield('content')
 
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
