d1633d2c6ccf226eb42192c1bd74fa37fecf953e
aschi87 <ernst.leisi@hotmail.com>
2015-05-22 09:15:21 +0200

    refactored

--- resources/views/budgetplan.blade.php
+++ resources/views/budgetplan.blade.php
@@ -5,17 +5,8 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Hier können Sie einen Budgetplan erstellen</div>
+                    <div class="panel-heading">Diese Seite ist inzwischen ÜBERFLÜSSIIIIIIIIG</div>
 
-                    <div class="panel-body">
-                        <form class="form-inline" action="" method="POST">
-                            <div class="form-group">
-                                <label for="budgetPlanName">Budgetplan-Name:</label>
-                                <input id="budgetPlanName" type="text" class="form-control" name="budgetPlanName" required> </input>
-                            </div>
-                            <button class="btn btn-default" type="button" action="submit">Hinzufügen</button>
-                        </form>
-                    </div>
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Budget-Übersicht
-                        <button type="submit" class="btn btn-default pull-right" >
+                        <button id="btnAddBudget" type="button" class="btn btn-default pull-right"  data-toggle="modal" data-target="#addBudgetModal">
                             <i class="glyphicon glyphicon-plus-sign"></i>
                         </button>
                 </div>
@@ -30,6 +30,37 @@
                         @endforeach
                     @endif
 				</div>
+
+                <div class="modal fade" id="addBudgetModal" tabindex="-1" role="dialog" aria-labelledby="addBudgetModal" data-target="#addBudget" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                                </button>
+                                <h4 class="modal-title">Neuer Budgetplan</h4>
+                            </div>
+                            <div class="modal-body">
+                                <form class="form-horizontal" id="addBudgetForm" method="POST">
+                                    <div class="form-group">
+                                        <div class="form-group">
+                                            <label class="col-sm-4 control-label">Name:</label>
+                                            <div class="col-sm-8">
+                                                <input type="text" id="name" name="name" required> </input>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <input type="hidden" name="_token" value="{{ Session::token() }}"> </input>
+                                </form>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" onclick="$('#addBudgetForm').submit();">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 			</div>
 		</div>
 	</div>
